3c8e8ff251102a8b8bb67364d6a8e2df7aabfe7e
PrestoshTJ <prestoshtj@gmail.com>
2024-10-08 21:41:15 -0400

    Pretty good progress. To do - Sort by attribute in stats. Be able to create packs too. (Need to change packInfo array infrastructure to do this)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
-import presidents from './cards';
-import React, { useState } from 'react';
+import packsArr from './cards';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-
+  const [packIndex, setPackIndex] = useState(1)
   const [item1, setItem1] = useState('Item 1');
   const [item2, setItem2] = useState("Item 2")
+  const [choices, setChoices] = useState([])
+  const [rankPage, setRankPage] = useState("Hidden")
+  const [packPage, setPackPage] = useState("Hidden")
+  const topItems = packsArr[packIndex]
+    .sort((a, b) => b.elo - a.elo) 
+    .slice(0, 5)
+  
+  useEffect(() => {
+    start()
+  }, [packIndex])
 
-  const cardPicked = (card) => {
-    console.log(card)
+  const cardPicked = (index, other) => {
+    let win = choices[index]
+    let lose = choices[other]
+    console.log(choices)
+    let winner = packsArr[packIndex][win].elo
+    let loser = packsArr[packIndex][lose].elo
+
+    let losePer = 1 / (1 + 10 ** ((winner - loser) / 400))
+    let winPer = 1 / (1 + 10 ** ((loser - winner) / 400))
+    let winELO = winner + 30 * (1 - winPer)
+    let loseELO = loser + 30 * (0 - losePer)
+
+    packsArr[packIndex][win].elo = winELO
+    packsArr[packIndex][lose].elo = loseELO
+    localStorage.setItem('packs', JSON.stringify(packsArr))
+    start()
+  }
+
+  const reset = () => {
+    for (let i = 0; i < packsArr[packIndex].length; i++) {
+      packsArr[packIndex].elo = 1000
+    }
+    localStorage.setItem('packs', JSON.stringify(packsArr))
+    window.location.reload();
+  }
+
+  const stats = () => {
+    if (rankPage == "Hidden") {
+      setRankPage("Shown")
+    }
+    else {
+      setRankPage("Hidden")
+    }
+  }
+
+  const packs = () => {
+    if (packPage == "Hidden") {
+      setPackPage("Shown")
+    } else {
+      setPackPage("Hidden")
+    }
   }
 
   const start = () => {
-    console.log(presidents)
-    let choices = []
-    let max = presidents.length 
+    setChoices([])
+    let max = packsArr[packIndex].length 
     let choice1 = Math.floor(Math.random() * max)
     let choice2 = Math.floor(Math.random() * max)
-    choices.push(choice1, choice2)
+    setChoices([choice1, choice2])
     if (choice1 == choice2) {
       start()
     } 
-    let data = presidents[choice1].name
+    let data = packsArr[packIndex][choice1].name
     setItem1(data)
-    data = presidents[choice2].name
+    data = packsArr[packIndex][choice2].name
     setItem2(data)
+  }
+
+  const packPick = (index) => {
+    setPackIndex(index)
+    setPackPage("Hidden")
   }
 
   return (
@@ -34,20 +87,37 @@ function App() {
         <div className = "new">
           <button onClick = {start}>Start</button>
         </div>
-        <div className = "reset">
+        <div className = "reset" onClick = {reset}>
           <button>Reset</button>
         </div>
-        <div className = "stats">
+        <div className = "stats" onClick = {stats}>
           <button>Stats</button>
+        </div>
+        <div className = "packs" onClick = {packs}>
+          <button>Packs</button>
         </div>
       </div>
       <div className = "cards">
-        <div className = "card" onClick={() => cardPicked(0)}>
+        <div className = "card" onClick={() => cardPicked(0,1)}>
           <h1 className = "item1" id = "item">{item1}</h1>
         </div>
-        <div className = "card" onClick={() => cardPicked(1)}>
+        <div className = "card" onClick={() => cardPicked(1,0)}>
           <h1 className = "item2" id = "item">{item2}</h1>
         </div>
+      </div>
+      <div id = "statsPage" className = {rankPage}>
+        <ul>
+          {topItems.map(item => (
+            <li key = {item.name}>{item.name} - {Math.floor(item.elo)}</li>
+          ))}
+        </ul>
+      </div>
+      <div id = "packsPage" className = {packPage}>
+          <ul>
+            {packsArr[0].map((pack, index) => (
+              <button onClick = {() => packPick(index + 1)}>{pack}</button>
+            ))}
+          </ul>
       </div>
     </div>
   );
